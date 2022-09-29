cbeb32a4d2a997797be862665f1be0d601c9f45c
Sergei S <div@altertech.com>
2022-09-29 23:36:55 +0200

    fmt

--- obsolete.sh
+++ obsolete.sh
@@ -5,14 +5,14 @@ EVA ICS v3 repository has been moved to https://pub.bma.ai/eva3
 
 Please use the new command to install new EVA ICS v3 nodes:
 
-curl https://pub.bma.ai/eva3/install | sh /dev/stdin [args...]
+  curl https://pub.bma.ai/eva3/install | sh /dev/stdin [args...]
 
 More info at: https://info.bma.ai/en/actual/eva3/install.html#installing
 
 To update v3 nodes with builds BEFORE v3.4.2 build 2022092901, use the
 following command (once):
 
-eva update -u https://pub.bma.ai/eva3
+  eva update -u https://pub.bma.ai/eva3
 
 More info at: https://info.bma.ai/en/actual/eva3/install.html#using-eva-shell
 
